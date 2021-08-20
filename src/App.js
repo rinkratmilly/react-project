7f1e07e9c7b061232986ba82fc14ebf9c026fc77
rinkratmilly <patrick.milligan4@gmail.com>
2021-08-20 10:43:48 -0400

    saftey commit

--- src/App.js
+++ src/App.js
@@ -4,30 +4,37 @@ import Main from './components/Main';
 import Aside from './components/Aside';
 import './App.css';
 
+
 function App () {
   const [characters, setCharacters] = useState (null);
-  const [loading, setLoading] = useState(true)
-  
+  const [loading, setLoading] = useState (true);
+  const [quote, setQuote] = useState ();
+
   useEffect (() => {
+    // !!!Below is format for auth in documentation!!!
+    // const headers = {
+    //   'Accept': 'application/json',
+    //   'Authorization': "Bearer "
+    // }
     const charReturn = {
       key: process.env.REACT_APP_PROJECT_KEY,
       limit: 9,
       api: 'https://the-one-api.dev/v2',
+      // endpoint: '/character/5cd99d4bde30eff6ebccfc42',
       endpoint: '/character',
     };
-    
+
     function getCharacters () {
       const url = `${charReturn.api}${charReturn.endpoint}`;
-  
+      // !!!Below was instructed by jexxe for auth/api call!!!
       fetch (url, {
         method: 'GET',
-        headers: {Authorization: `Bearer ${charReturn.key}`},
+        headers: {Authorization: `Bearer ${process.env.REACT_APP_PROJECT_KEY}`},
       })
         .then (res => res.json ())
         .then (res => {
           setCharacters (res);
-          setLoading(false)
-          console.log ();
+          setLoading (false);
         });
     }
     getCharacters ();
@@ -44,8 +51,8 @@ function App () {
 
       <main>
 
-        <Main characters={characters}
-              loading={loading} />
+        <Main characters={characters} loading={loading} quote={quote}
+         />
 
       </main>
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
 function Main ({characters, loading}) {
-    console.log(characters)
     if(loading){
         return <h1>loading</h1>
     } else {
-        
+        console.log(characters)
+  
         return (
           <div>
-            {/* <h1>{characters.docs[0].name}</h1> */}
-      
+            <h1>{characters.docs.map(character => {
+              return(
+                <h1>{character.name}</h1>
+              );
+            })
+            }
+
           </div>
         );
     }
-
+   
 }
 
 export default Main;
